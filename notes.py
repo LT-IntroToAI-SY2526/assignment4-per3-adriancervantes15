802924c9a80c97f393ffd1a1bcbe1c1717bced5a
Adrian Cervantes Lopez <acervantes-2@cps.edu>
2025-10-23 10:36:08 -0500

    firstDay

--- notes.py
+++ notes.py
@@ -2,3 +2,14 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    """A simple Dog class to learn OOP
+    
+    Attributes: 
+        fir_col - the color o the dogs fur 
+        name = the name of the dog 
+        age - the age of the dog
+        favorite_food - the dogs favoite food
+    """
+    def __init__(self, fur_color, name, age, favorite_food):
+        pass
