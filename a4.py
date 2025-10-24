c8ff56cc020797e1ad3a1e9d1463b5bdee995a83
Adrian Cervantes Lopez <acervantes-2@cps.edu>
2025-10-24 10:35:12 -0500

    secondDay

--- a4.py
+++ a4.py
@@ -22,10 +22,59 @@ class TTTBoard:
         return boardStr
     
     def make_move(self, player, pos):
+       if self.board[pos] == "*":
+           self.board[pos] = player
+           return True
+       else:
+           return False
+    
+    def has_won(self, player):
+        if self.board[0] == self.board[1] == self.board[2] != "*":
+            return True
+        elif self.board[3] == self.board[4] == self.board[5] != "*":
+            return True
+        elif self.board[6] == self.board[7] == self.board[8] != "*":
+            return True
+        elif self.board[0] == self.board[3] == self.board[6] != "*":
+            return True
+        elif self.board[1] == self.board[4] == self.board[7] != "*":
+            return True
+        elif self.board[2] == self.board[5] == self.board[8] != "*":
+            return True
+        elif self.board[0] == self.board[4] == self.board[8] != "*":
+            return True
+        elif self.board[2] == self.board[4] == self.board[6] != "*":
+            return True
+        else:
+            return False
+        
+    def game_over(self):
+        if self.board[0] == self.board[1] == self.board[2] != "*":
+            return True
+        elif self.board[3] == self.board[4] == self.board[5] != "*":
+            return True
+        elif self.board[6] == self.board[7] == self.board[8] != "*":
+            return True
+        elif self.board[0] == self.board[3] == self.board[6] != "*":
+            return True
+        elif self.board[1] == self.board[4] == self.board[7] != "*":
+            return True
+        elif self.board[2] == self.board[5] == self.board[8] != "*":
+            return True
+        elif self.board[0] == self.board[4] == self.board[8] != "*":
+            return True
+        elif self.board[2] == self.board[4] == self.board[6] != "*":
+            return True
+        elif self.board[0] != "*" and self.board[1] != "*" and self.board[2] != "*" and self.board[3] != "*" and self.board[4] != "*" and self.board[5] != "*" and self.board[6] != "*" and self.board[7] != "*" and self.board[8] != "*":
+            return True
+        else:
+            return False
 
 
-def play_tic_tac_toe() -> None:
-    """Uses your class to play TicTacToe"""
+
+
+    def play_tic_tac_toe() -> None:
+        """Uses your class to play TicTacToe"""
 
     def is_int(maybe_int: str):
         """Returns True if val is int, False otherwise
@@ -42,29 +91,29 @@ def play_tic_tac_toe() -> None:
         except ValueError:
             return False
 
-    brd = TTTBoard()
-    players = ["X", "O"]
-    turn = 0
+brd = TTTBoard()
+players = ["X", "O"]
+turn = 0
 
-    while not brd.game_over():
-        print(brd)
-        move: str = input(f"Player {players[turn]} what is your move? ")
+while not brd.game_over():
+    print(brd)
+    move: str = input(f"Player {players[turn]} what is your move? ")
 
-        if not is_int(move):
-            raise ValueError(
-                f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
-            )
+    if not is_int(move):
+        raise ValueError(
+            f"Given invalid position {move}, position must be integer between 0 and 8 inclusive"
+        )
 
-        if brd.make_move(players[turn], int(move)):
-            turn = not turn
+    if brd.make_move(players[turn], int(move)):
+        turn = not turn
 
-    print(f"\nGame over!\n\n{brd}")
-    if brd.has_won(players[0]):
-        print(f"{players[0]} wins!")
-    elif brd.has_won(players[1]):
-        print(f"{players[1]} wins!")
-    else:
-        print(f"Board full, cat's game!")
+print(f"\nGame over!\n\n{brd}")
+if brd.has_won(players[0]):
+    print(f"{players[0]} wins!")
+elif brd.has_won(players[1]):
+    print(f"{players[1]} wins!")
+else:
+    print(f"Board full, cat's game!")
 
 
 if __name__ == "__main__":
